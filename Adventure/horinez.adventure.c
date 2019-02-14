36c5a3968e05be786d135eb499e98844cf64c865
Zachary Horine <horine.zachary@gmail.com>
2019-02-13 22:22:51 -0800

    rearrange imports

--- Adventure/horinez.adventure.c
+++ Adventure/horinez.adventure.c
@@ -7,11 +7,13 @@
 
 #include <unistd.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 #include <string.h>
 #include <dirent.h>
-#include <sys/stat.h>
+#include <pthread.h>
 
 typedef int bool;
 #define true 1
