609d635629417aa98e2d4ef3a727edd56dc40002
Zachary Horine <horine.zachary@gmail.com>
2019-02-13 16:17:18 -0800

    find most recent room folder

--- Adventure/horinez.adventure.c
+++ Adventure/horinez.adventure.c
@@ -53,4 +53,29 @@ void *timer() {
 
 
 void *game() {
+   DIR topLvlDir = opendir(".");
+   char curdir[100];
+   char filename[100];
+   struct dirent newestdir;
+   struct dirent checkdir = readdir(topLvlDir);
+   time_t newestdirTime = 0;
+
+   while (dir != NULL) {
+      struct stat res;
+      stat(checkdir->d_name,res);
+      time_t lastmod = res.st_mtime;
+      if (S_ISDIR(res.st_mode) && checkdir->d_name[0] != '.') { //is a directory and is not the "." directory
+         if lastmod > newestdirTime ) { //current dir time is newer than previous best
+            newestdir = checkdir;
+            newestdirTime = lastmod;
+         }
+      }
+      checkdir = readdir(topLvlDir);
+   }
+   closedir(topLvlDir);
+
+   if (newestdirTime == 0) {
+      printf("ERR: NO ROOM FOLDERS DETECTED");
+      exit(1);
+   }
 }
