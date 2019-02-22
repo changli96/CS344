ad86c38a1e1420562ca49dd7b9db5468fc2683df
Zachary Horine <horine.zachary@gmail.com>
2019-02-22 11:46:43 -0800

    reduce code to more elegant solution

--- Python Py/mypython.py
+++ Python Py/mypython.py
@@ -10,17 +10,10 @@ def generate_string(numChars):
     sys.stdout.write(str);
     return str;
 
+file1 = open("file1",'w').write(generate_string(10));
+file2 = open("file2",'w').write(generate_string(10));
+file3 = open("file3",'w').write(generate_string(10));
 
-file1 = open("file1",'w');
-file2 = open("file2",'w');
-file3 = open("file3",'w');
-
-file1.write(generate_string(10));
-file2.write(generate_string(10));
-file3.write(generate_string(10));
-file1.close();
-file2.close();
-file3.close();
 a = random.randint(1,42);
 b = random.randint(1,42);
 sys.stdout.write(str(a)+"\n"+str(b)+"\n"+str(a*b)+"\n");
