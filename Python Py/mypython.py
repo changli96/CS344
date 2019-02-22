36597726f30ce586deff3cafd9f68916bdbe6e2e
Zachary Horine <horine.zachary@gmail.com>
2019-02-22 11:50:38 -0800

    minimum lines!

--- Python Py/mypython.py
+++ Python Py/mypython.py
@@ -1,19 +1,11 @@
 import random;
 import sys;
-random.seed();
-
-def generate_string(numChars):
-    str = "";
-    for i in range(numChars):
+str = "";
+for f in range 3:
+    for i in range(10):
         str = str + chr(random.randint(97, 122));
-    str = str + "\n";
-    sys.stdout.write(str);
-    return str;
-
-file1 = open("file1",'w').write(generate_string(10));
-file2 = open("file2",'w').write(generate_string(10));
-file3 = open("file3",'w').write(generate_string(10));
-
+    sys.stdout.write(str + "\n");
+open("file"+str(f),'w').write(str + "\n");
 a = random.randint(1,42);
 b = random.randint(1,42);
 sys.stdout.write(str(a)+"\n"+str(b)+"\n"+str(a*b)+"\n");
