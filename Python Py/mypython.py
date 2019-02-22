0073fddcd7f509447920c95c4c419180be5000f5
Zachary Horine <horine.zachary@gmail.com>
2019-02-22 12:16:43 -0800

    fix bugs

--- Python Py/mypython.py
+++ Python Py/mypython.py
@@ -1,11 +1,11 @@
 import random;
 import sys;
-str = "";
-for f in range 3:
+for f in range(3):
+    string = "";
     for i in range(10):
-        str = str + chr(random.randint(97, 122));
-    sys.stdout.write(str + "\n");
-open("file"+str(f),'w').write(str + "\n");
+        string = string + chr(random.randint(97, 122));
+    sys.stdout.write(string + "\n");
+    open("file"+str(f),'w').write(string + "\n");
 a = random.randint(1,42);
 b = random.randint(1,42);
 sys.stdout.write(str(a)+"\n"+str(b)+"\n"+str(a*b)+"\n");
