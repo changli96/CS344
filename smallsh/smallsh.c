71a4a27908e168afa0c68e2a407bcec94ec8ab89
Zachary Horine <horine.zachary@gmail.com>
2019-02-28 06:51:07 -0800

    basic outline of program

---
+++ smallsh/smallsh.c
@@ -0,0 +1,29 @@
+//##################\\
+//##ZACHARY HORINE##\\
+//##CS344---BLOCK2##\\
+//##WINTER02132019##\\
+//##################\\
+//smallsh.c
+int main() {
+
+}
+
+void cd_cmd() {
+
+}
+
+void exit_cmd() {
+   exit(0);
+}
+
+void status_cmd() {
+
+}
+
+void parseCmd() {
+}
+
+//this method takes in a command string and executes the given command with its arguments
+int runChild() {
+
+}
