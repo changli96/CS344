5822e5c2bef8a6c6d8f70ad7031279994980af43
Zachary Horine <horine.zachary@gmail.com>
2019-03-07 15:05:28 -0800

    update keygen to print errors to stderr

--- otp/keygen.c
+++ otp/keygen.c
@@ -12,10 +12,10 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-   if (argc < 2) {printf("No keylength given!\n"); exit(1);}
-   if (argc > 2) {printf("Too many arguments!\n"); exit(1);}
+   if (argc < 2) {fprintf(stderr,"No keylength given!\n"); exit(1);}
+   if (argc > 2) {fprintf(stderr,"Too many arguments!\n"); exit(1);}
    int keylength = atoi(argv[1]);
-   if (keylength == 0) {printf("Please input a number.\n"); exit(1);}
+   if (keylength == 0) {fprintf(stderr,"Please input a number.\n"); exit(1);}
    char chars[28] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
    srand(time(NULL));   // random initialization
    int i;
