fddf3a972495064aa89ab6ea93887892477c77c5
Zachary Horine <horine.zachary@gmail.com>
2019-03-17 20:57:05 -0700

    attempt 1

---
+++ otp/otp_enc.c
@@ -0,0 +1,175 @@
+//##################\\
+//##ZACHARY HORINE##\\
+//##CS344---BLOCK4##\\
+//##WINTER03172019##\\
+//##################\\
+//otp_enc.c
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int testChar(int c);
+
+int main(int argc, char *argv[]) {
+   int curchar;
+   long plaintextSize = -1; //plaintext length
+   long keySize = -1; //key lenth
+
+   if (argc != 4) {fprintf(stderr,"Usage: otp_enc [plaintext] [key] [port]\n"); exit(1);}
+   FILE *plaintextFile = fopen(argv[1],"rb");
+   if (plaintextFile == NULL) {fprintf(stderr, "the file \"%s\" is unavailable\n", argv[1]); exit(1);}
+   fseek(plaintextFile,0,SEEK_END);
+   plaintextSize = ftell(plaintextFile)+1;
+   fclose(plaintextFile);
+   // Verify that we have good text in files
+   plaintextFile = fopen(argv[1],"r"); //check for invalid characters
+   while((curchar = fgetc(plaintextFile)) != EOF){
+      if(!testChar(curchar)){
+         fprintf(stderr,"Error: invalid chars in plaintext\n");
+         exit(1);
+      }
+   }
+   fclose(plaintextFile); //reset plaintext file pointer
+   plaintextFile = fopen(argv[1],"r");
+
+   FILE *keyFile = fopen(argv[2],"rb");
+   if (keyFile == NULL) {fprintf(stderr, "the file \"%s\" is unavailable\n", argv[2]); exit(1);}
+   fseek(keyFile,0,SEEK_END);
+   keySize = ftell(keyFile)+1;
+   fclose(keyFile);
+   keyFile = fopen(argv[2],"r"); //check for invalid characters
+   while((curchar = fgetc(keyFile)) != EOF){
+      if(!testChar(curchar)){
+         fprintf(stderr,"Error: invalid chars in key\n");
+         exit(1);
+      }
+   }
+   fclose(keyFile); //reset key file pointer
+   keyFile = fopen(argv[2],"r");
+
+   //set port
+   int port = atoi(argv[3]);
+   if(port <= 0 || port > 65535) {fprintf(stderr, "the port you entered \"%s\" is invalid. (Range: 1-65535)\n", argv[3]); exit(1);}
+
+
+   int i, j;
+   int charsWritten;
+   int charsRead;
+   struct sockaddr_in serverAddress;
+   struct hostent* serverHostInfo;
+   int packetSize = 512;
+   char buffer[packetSize];
+   char *local = "localhost";
+
+
+   int numSegments = plaintextSize/packetSize-1 + 1;// calculate number of packets to create
+   memset(buffer, '\0', sizeof(buffer)); // Clear buffer
+
+   if(plaintextSize > keySize){// Check to make sure that the key is not shorter than the plaintext
+      fprintf(stderr,"Error: key must be longer than the message\n");
+      exit(1);
+   }
+
+   for(i=0;i<sizeof(buffer);i++){
+      buffer[i] = 0;
+   }
+
+
+   // Set up the server address struct
+   memset((char*)&serverAddress, '\0', sizeof(serverAddress));
+   serverAddress.sin_family = AF_INET; // create socket
+   serverAddress.sin_port = htons(port); // set the port
+   serverHostInfo = gethostbyname(local); // set host name
+   if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+   memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // set the address
+
+   // Set up the socket
+   int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+   if (socketFD < 0) {fprintf(stderr, "Error: could not open socket"); exit(1);}
+
+   // Connect to server
+   if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+      {fprintf(stderr, "Error: could not connect to socket"); exit(1);}
+
+   strcpy(buffer,"otp_enc"); //send id
+   charsWritten = send(socketFD, buffer, strlen(buffer), 0); // Write to the server who we are
+   if (charsWritten < 0) {fprintf(stderr, "Error: could not write to socket"); exit(1);}
+   if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+   memset(buffer, '\0', sizeof(buffer));
+   charsRead = recv(socketFD, buffer, 3, 0); // Wait for ack
+   if (charsRead < 0) {fprintf(stderr, "Error: could not read from socket"); exit(1);}
+   if (strcmp(buffer,"ack")){fprintf(stderr,"Error bad port");exit(1);}// no return ack
+
+   sprintf(buffer,"%d",numSegments);
+   charsWritten = send(socketFD, buffer, strlen(buffer), 0); // send number of segments
+   if (charsWritten < 0) {fprintf(stderr, "Error: could not write to socket"); exit(1);}
+   if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+   // Send the key then plaintext
+   for(i=0;i<numSegments;i++){
+
+      //key
+      memset(buffer, '\0', packetSize);
+      fgets(buffer,packetSize,keyFile);
+      // Fill the packet with * if it isn't a full packet
+      if(i == numSegments-1){
+         for(j=0;j<packetSize-1;j++){
+            if(buffer[j] == '\0' || buffer[j] == '\n'){buffer[j] = '*';}
+         }
+      }
+      //Send the packet while there are still bits to send
+      while((charsWritten = send(socketFD, buffer, strlen(buffer), 0)) != packetSize-1){}
+      if (charsWritten < 0) {fprintf(stderr, "Error: could not write to socket"); exit(1);}
+      if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+      //plaintext
+      memset(buffer, '\0', packetSize);
+      fgets(buffer,packetSize,plaintextFile);
+      // Fill the packet with * if it isn't a full packet
+      if(i == numSegments-1){
+         for(j=0;j<packetSize-1;j++){
+            if(buffer[j] == '\0' || buffer[j] == '\n'){buffer[j] = '*';}
+         }
+      }
+      //Send the packet while there are still bits to send
+      while((charsWritten = send(socketFD, buffer, strlen(buffer), 0)) != packetSize-1){}
+      if (charsWritten < 0) {fprintf(stderr, "Error: could not write to socket"); exit(1);}
+      if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+
+      memset(buffer, '\0', sizeof(buffer));
+      while((charsRead = recv(socketFD, buffer, packetSize-1, 0) < packetSize-1)){} // Read data, leave \0
+      if (charsRead < 0) {fprintf(stderr, "Error: could not read from socket"); exit(1);}
+      if(i == numSegments-1){
+         for(j=0;j<strlen(buffer);j++){
+            if(buffer[j] != '*' && testChar(buffer[j])){
+               printf("%c", buffer[j]);
+            }
+            else{break;}
+         }
+      }
+      else{printf("%s",buffer);}
+   }
+   printf("\n");
+   return 0;
+}
+
+int testChar(int c){
+   int i = 0;
+   char chars[28] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+   for(i=0;i<27;i++){
+      if(c == chars[i]){// It's a valid char
+         return 1;
+      }
+   }
+   if(c == 88 || c == 10){//end of file chars
+      return 1;
+   }
+   return 0;
+}
---
+++ otp/otp_enc_d.c
@@ -0,0 +1,145 @@
+//##################\\
+//##ZACHARY HORINE##\\
+//##CS344---BLOCK4##\\
+//##WINTER03172019##\\
+//##################\\
+//otp_enc_d.c
+
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]) {
+   if (argc != 2) {fprintf(stderr,"Usage: otp_enc_d [port]\n"); exit(1);}
+
+   char chars[28] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+   int packetSize = 512;
+   socklen_t sizeOfClientInfo;
+
+   struct sockaddr_in serverAddress, clientAddress;
+   memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+   int portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+   serverAddress.sin_family = AF_INET; // Create a network-capable socket
+   serverAddress.sin_port = htons(portNumber); // Store the port number
+   serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
+
+   // Set up socket
+   int listenSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+   if (listenSocketFD < 0) {
+      fprintf(stderr,"Error: Could not open socket\n"); exit(1);
+   }
+
+   // Enable socket and start listening
+   if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+      fprintf(stderr,"Error: Could not bind socket to port %d\n", portNumber); exit(1);
+   listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+
+   int establishedConnectionFD, charsRead,numPackets;
+   char keybuffer[packetSize];// For storeing key packets
+   while(1){
+      // Accept a connection, blocking if one is not available until one connects
+      sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+      establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+      if (establishedConnectionFD < 0) {fprintf(stderr,"Error: Did not accept\n"); exit(1);}
+
+      int pid = fork();
+      if(pid == -1){// Failed to create child
+         exit(1);
+      }
+      else if(pid == 0){// Child process
+         //Preform our two checks
+         //The first packet we get should be "otp_enc"
+         memset(keybuffer, '\0', packetSize);
+         charsRead = recv(establishedConnectionFD, keybuffer, 7, 0); // Read the client's message from the socket
+         if (charsRead < 0) {fprintf(stderr,"Error: Could not read from socket\n"); exit(1);}
+         //printf("SERVER: I received this from the client: \"%s\"\n", keybuffer);
+         if(!strcmp(keybuffer,"otp_enc")){
+            charsRead = send(establishedConnectionFD, "ack", 3, 0); // Send success back
+            if (charsRead < 0) {fprintf(stderr,"Error: Could not write to socket\n"); exit(1);}
+         }
+         else{
+            charsRead = send(establishedConnectionFD, "fek", 3, 0); // Tell the process to kill it'self then kill ourself
+            if (charsRead < 0) {fprintf(stderr,"Error: Could not write to socket\n"); exit(1);}
+            close(establishedConnectionFD); // Close the existing socket which is connected to the client
+            close(listenSocketFD); // Close the listening socket
+            exit(1);
+         }
+
+
+         //The second packet is the number of packet we are expecting
+         memset(keybuffer, '\0', packetSize);
+         charsRead = recv(establishedConnectionFD, keybuffer, 7, 0); // Read the client's message from the socket
+         if (charsRead < 0) {fprintf(stderr,"Error: Could not read from socket\n"); exit(1);}
+         numPackets = atoi(keybuffer);
+         numPackets = numPackets;
+         //printf("SERVER: I received this from the client: \"%d\"\n", numPackets);
+
+         char plaintext[packetSize];
+         // Get the message from the client and display it
+         while(numPackets > 0){// The connection is open if we get an end of transmission packet then set on = 0
+
+            //Grab the key
+            memset(keybuffer, '\0', packetSize);
+            while((charsRead = recv(establishedConnectionFD, keybuffer, packetSize-1, 0)) < packetSize-1){} // Read the client's message from the socket
+            if (charsRead < 0) {fprintf(stderr,"Error: Could not read from socket\n"); exit(1);}
+            if(charsRead > 254){
+               //printf("SERVER: I received this from the client: \"%s\"\n%d\n\n", keybuffer,strlen(keybuffer));
+            }
+            else{
+               printf("Server: Packet droped too small\n\n");
+            }
+
+            //Grab the plaintext
+            memset(plaintext, '\0', packetSize);
+            while((charsRead = recv(establishedConnectionFD, plaintext, packetSize-1, 0)) < packetSize-1){} // Read the client's message from the socket
+            if (charsRead < 0) {fprintf(stderr,"Error: Could not read from socket\n"); exit(1);}
+            if(charsRead > 254){
+               //printf("SERVER: I received this from the client: \"%s\"\n%d\n\n", plaintext,strlen(plaintext));
+            }
+            else{
+               printf("Server: Packet droped too small\n\n");
+            }
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            char cypher[packetSize];
+            //Send back the encrypted text
+            memset(cypher, '\0', packetSize);
+            for(i=0;i<packetSize;i++){
+               if(plaintext[i] == '*'){ //end of last packet filled with '*' chars.
+                  break;
+               }
+               j = keybuffer[i] - 65;// Key
+               k = plaintext[i] - 65;// Plaintext
+               if(j == -33){j=26;}// For " "
+               if(k == -33){k=26;}
+               if(j >= 0 && k >= 0){
+                  cypher[i] = chars[(j + k) % 27];
+               }
+            }
+            if(numPackets == 1){
+               for(j=0;j<packetSize-1;j++){
+                  if(cypher[j] == '\0' || cypher[j] == '\n'){
+                     cypher[j] = '*';
+                  }
+               }
+            }
+
+            //Send a Success message back to the client
+            while((charsRead = send(establishedConnectionFD, cypher, strlen(cypher), 0)) != packetSize-1){} // Send success back
+            if (charsRead < 0) {fprintf(stderr,"Error: Could not write to socket\n"); exit(1);}
+            numPackets--;
+         }
+         close(establishedConnectionFD); // Close the existing socket which is connected to the client
+         return 0;
+      }
+      close(establishedConnectionFD); // Close the existing socket which is connected to the client
+   }
+   close(listenSocketFD); // Close the listening socket
+}
